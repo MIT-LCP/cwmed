476ee114a4167c7bf963bc8e00f1bdbac913b0f7
xborrat <xborrat@yahoo.es>
2022-10-05 12:33:36 -0400

    second round fix flake8 issues

--- crosswalk.py
+++ crosswalk.py
@@ -19,7 +19,7 @@ class Standard_vocab_translator(object):
         source_vocabulary (str): Filename of the source vocabulary.
         target_vocabulary (str): Filename of the target vocabulary.
         source_file (str): Path to the source vocabulary.
-        source_file_code_column (str): Column in source vocabulary that will 
+        source_file_code_column (str): Column in source vocabulary that will
             be mapped to the target vocabulary.
 
     Attributes:
@@ -32,69 +32,70 @@ class Standard_vocab_translator(object):
 
     """
 
-    def __init__(self, source_vocabulary: str, target_vocabulary: str, 
+    def __init__(self, source_vocabulary: str, target_vocabulary: str,
                  source_file: str, source_file_code_column: str):
         # Vocabulary Variables
         self.source_vocabulary = source_vocabulary
         self.target_vocabulary = target_vocabulary
-        
+
         # Concept dictionary load.
         concept = pd.read_csv("CONCEPT.csv", sep="\\t", error_bad_lines=False,
                               converters={"concept_id": str,
-                                          "concept_code": str}, engine='python')
+                                          "concept_code": str},
+                              engine='python')
         # List of all vocabularies in the diccionary
         self.vocab_list = concept["vocabulary_id"].unique()
-        
+
         # Concept relationship dictionary load.
         concept_rel = pd.read_csv(
             "CONCEPT_RELATIONSHIP.csv", sep="\\t",
             error_bad_lines=False,
             converters={"concept_id_1": str,
                         "concept_id_2": str}, engine='python')
-        
+
         # Select only the needed vocabularies.
-        
+
         # Example with 'NDC' and 'RxNorm'
         concept = concept[(concept["vocabulary_id"] == source_vocabulary)
                           | (concept["vocabulary_id"] == target_vocabulary)]
-       
-        # Select only the relationships on "mapping to". 
+
+        # Select only the relationships on "mapping to".
         concept_rel = concept_rel[concept_rel['relationship_id'] == "Maps to"]
-        
+
         # Load your source file.
-        source_voc = pd.read_csv(source_file, 
+        source_voc = pd.read_csv(source_file,
                                  converters={source_file_code_column: str})
         self.source_voc = source_voc
-        
-        # 1 Translate your source vocabulary code(concept_name) TO 
+
+        # 1 Translate your source vocabulary code(concept_name) TO
         # concept_id(NDC).
         # replace left_on value for the source code columns name.
         concept_id_source = source_voc.merge(
-            concept, how='left', 
+            concept, how='left',
             left_on=source_file_code_column, right_on="concept_code")
-        
+
         # 2 step. Merge tables joining on concept_id(from source voc) to
         #  concept_id()
         source_rel_merge = concept_id_source.merge(
             concept_rel, how='left',
-            left_on="concept_id", 
+            left_on="concept_id",
             right_on="concept_id_1")
-        
-        # Cleaning undesired columns 
-        source_rel_merge = source_rel_merge[[source_file_code_column, 
+
+        # Cleaning undesired columns
+        source_rel_merge = source_rel_merge[[source_file_code_column,
                                              'concept_id',
                                              'concept_id_1',
                                              'concept_id_2']]
-        
-        # step 3. Merge tables joining the concept_id_2(concept_id from target 
-        # voc) with concept dictionary to 
+
+        # step 3. Merge tables joining the concept_id_2(concept_id from target
+        # voc) with concept dictionary to
         # obabstain the concept_code of the target voc.
 
         rel_target_merge = source_rel_merge.merge(
-            concept, how='left', 
+            concept, how='left',
             left_on="concept_id_2",
             right_on="concept_id")
-        
+
         rel_target_merge = rel_target_merge[[
             source_file_code_column,
             'concept_id_x',
@@ -102,14 +103,14 @@ class Standard_vocab_translator(object):
             'concept_id_2',
             'concept_code']]
         # Renaming columns for clarity
-        
+
         rel_target_merge.columns = [
             source_file_code_column,
             'concept_id_x',
             'concept_id_{}'.format(source_vocabulary),
             'concept_id_{}'.format(target_vocabulary),
             target_vocabulary]
-        
+
         self.rel_target_merge = rel_target_merge
 
     def print_dic(self):
@@ -131,7 +132,7 @@ class Standard_vocab_translator(object):
     def failed_mappings(self):
         """Prints a table of failed mappings.
 
-        Prints a pd.DataFrame that lists concepts that could not be matched 
+        Prints a pd.DataFrame that lists concepts that could not be matched
         between the source and target vocabularies.
         """
         failed_mappings = self.rel_target_merge[self.rel_target_merge[
