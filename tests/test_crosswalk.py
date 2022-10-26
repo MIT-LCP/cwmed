491f19748f0cf357e6da3405975541eca001bdb9
danamouk <danamouk@buffalo.edu>
2022-10-26 14:19:48 -0400

    change class

--- tests/test_crosswalk.py
+++ tests/test_crosswalk.py
@@ -1,10 +1,10 @@
-from crosswalk import Standard_vocab_translator
+from crosswalk import VocabTranslator
 
 def test_crosswalk():
     """
     Test that the crosswalk is working.
     """
-    test = Standard_vocab_translator('NDC','RxNorm','source_2.csv','ndc')
+    test = VocabTranslator('NDC','RxNorm','source_2.csv','ndc')
     ndc_source=test.source_voc.ndc[16]
     ndc_source
     rxnorm_out_column=test.rel_target_merge[test.rel_target_merge.ndc==ndc_source]['RxNorm'].iloc[0]
