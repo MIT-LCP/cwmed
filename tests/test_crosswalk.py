ae1e0a9e2d72317d6f39215ac1890b4c9d9c7228
danamouk <danamouk@buffalo.edu>
2022-10-26 16:52:24 -0400

    updates test

--- tests/test_crosswalk.py
+++ tests/test_crosswalk.py
@@ -1,13 +1,20 @@
+import pytest
+
 from crosswalk import VocabTranslator
 
-def test_crosswalk():
+def test_crosswalk_between_ndc_rxnorm():
     """
-    Test that the crosswalk is working.
+    Test that an ndc code from the source file is mapped to the expected rxnorm code in the common vocabulary.
     """
-    test = VocabTranslator('NDC','RxNorm','source_2.csv','ndc')
-    ndc_source=test.source_voc.ndc[16]
-    ndc_source
-    rxnorm_out_column=test.rel_target_merge[test.rel_target_merge.ndc==ndc_source]['RxNorm'].iloc[0]
-    rxnorm_out_column
+    vocab = VocabTranslator('NDC','RxNorm','source_2.csv','ndc')
+    ndc_source=vocab.source_voc.ndc[16]
+    rxnorm_code=vocab.rel_target_merge[vocab.rel_target_merge.ndc==ndc_source]['RxNorm']
     expected_rxnorm = '313002'
-    assert expected_rxnorm == rxnorm_out_column
+    assert expected_rxnorm == rxnorm_code
+
+
+
+
+
+
+
