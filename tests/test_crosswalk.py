44a251d96bb744dcba935a06320eeb304638dfdc
danamouk <danamouk@buffalo.edu>
2022-10-26 12:27:13 -0400

    adds test to check crosswalk

--- tests/test_crosswalk.py
+++ tests/test_crosswalk.py
@@ -1,8 +1,13 @@
-import pytest
+from crosswalk import Standard_vocab_translator
 
-
-def test_pytest():
+def test_crosswalk():
     """
-    Test that pytest is working.
+    Test that the crosswalk is working.
     """
-    assert True
+    test = Standard_vocab_translator('NDC','RxNorm','source_2.csv','ndc')
+    ndc_source=test.source_voc.ndc[16]
+    ndc_source
+    rxnorm_out_column=test.rel_target_merge[test.rel_target_merge.ndc==ndc_source]['RxNorm'].iloc[0]
+    rxnorm_out_column
+    expected_rxnorm = '313002'
+    assert expected_rxnorm == rxnorm_out_column
