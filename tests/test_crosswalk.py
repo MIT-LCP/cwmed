37ed0e675c043ce2f7a7ede55f58c54535c86362
Dana <dana@Danas-MacBook-Pro.local>
2022-10-26 22:16:38 -0400

    updates test

--- tests/test_crosswalk.py
+++ tests/test_crosswalk.py
@@ -7,7 +7,7 @@ def test_crosswalk_between_ndc_rxnorm():
     Test that an ndc code from the source file is mapped to the expected rxnorm code in the common vocabulary.
     """
     vocab = VocabTranslator('NDC','RxNorm','source_2.csv','ndc')
-    ndc_source=vocab.source_voc.ndc[16]
-    rxnorm_code=vocab.rel_target_merge[vocab.rel_target_merge.ndc==ndc_source]['RxNorm']
+    ndc_source = vocab.source_voc.ndc[16]
+    rxnorm_code = vocab.rel_target_merge[vocab.rel_target_merge.ndc==ndc_source]['RxNorm'].iloc[0]
     expected_rxnorm = '313002'
     assert expected_rxnorm == rxnorm_code
