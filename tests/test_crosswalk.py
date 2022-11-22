f78786df5947a55ff9bade45f09208df7581bfc7
Dana <dana@Danas-MacBook-Pro.local>
2022-11-22 17:43:14 -0500

    adds function to read source file and updates test

--- tests/test_crosswalk.py
+++ tests/test_crosswalk.py
@@ -12,7 +12,8 @@ def test_crosswalk_between_icd10_snomed():
                                concept_filepath = 'tests/data/concept_example_icd10_snomed.csv',
                                source_vocab_value = 'ICD10CM',target_vocab_value = 'SNOMED',
                                concept_relationship_filepath = 'tests/data/concept_relationship_example_icd10_snomed.csv')
-    icd10_source_code = vocab.source_voc[vocab.source_voc.icd10 == 'A04.4']['icd10'].iloc[0]
+    source_df = vocab._VocabTranslator__read_source_file()
+    icd10_source_code = source_df[source_df.icd10 == 'A04.4']['icd10'].iloc[0]
     snomed_target_code = vocab.rel_target_merge[vocab.rel_target_merge.icd10 == icd10_source_code]['SNOMED'].iloc[0]
     expected_target_code = '111839008'  
     assert expected_target_code == snomed_target_code
